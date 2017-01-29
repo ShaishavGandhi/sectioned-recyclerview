4b03d4675a3af820b067bff6580bca5f258dec09
shaishavgandhi05 <shaishgandhi@gmail.com>
2017-01-29 13:23:07 -0800

    Change Package Name
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

---
+++ app/src/main/java/com/shaishavgandhi/shaishavgandhi/samplesectionedrecyclerview/sections/MoviesHeader.java
@@ -0,0 +1,48 @@
+package com.shaishavgandhi.shaishavgandhi.samplesectionedrecyclerview.sections;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.shaishavgandhi.sectionedrecyclerview.Section;
+import com.shaishavgandhi.shaishavgandhi.samplesectionedrecyclerview.R;
+
+/**
+ * Created by shaishav.gandhi on 11/23/16.
+ */
+
+public class MoviesHeader extends Section<MoviesHeader.MiscSectionHolder> {
+
+    public MoviesHeader() {
+    }
+
+    @Override
+    public void onBind(MiscSectionHolder holder, int position) {
+        // Do nothing
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.list_item_header;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+    @Override
+    public MiscSectionHolder getViewHolder(View view) {
+        return new MiscSectionHolder(view);
+    }
+
+    public static class MiscSectionHolder extends RecyclerView.ViewHolder {
+
+        TextView mView;
+
+        public MiscSectionHolder(View itemView) {
+            super(itemView);
+            mView = (TextView) itemView.findViewById(R.id.header);
+        }
+    }
+}
